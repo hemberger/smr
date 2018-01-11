d0a4bdcbd1d3777d2c0a4b2abb781ba3e63795ab
Dan Hemberger <daniel.hemberger@gmail.com>
2018-01-10 16:38:46 -0800

    Add class SmrTradeGood
    This replaces the global goods array.
    
    A few extra notes:
    * Remove references to tier ("class") 4 goods. There are none.

--- engine/Default/cargo_dump.php
+++ engine/Default/cargo_dump.php
@@ -1,6 +1,8 @@
 <?php
 $template->assign('PageTopic','Dump Cargo');
 
+require_once(LIB . 'Default/SmrTradeGood.class.inc');
+
 $db->query('SELECT * FROM ship_has_cargo JOIN good USING(good_id)
 			WHERE account_id = ' . $db->escapeNumber($player->getAccountID()) . '
 				AND game_id = ' . $db->escapeNumber($player->getGameID()));
@@ -13,7 +15,7 @@ if ($db->getNumRows()) {
 		$container['good_id'] = $db->getInt('good_id');
 
 		$goods[] = array(
-			'image' => Globals::getGood($db->getInt('good_id'))['ImageLink'],
+			'image' => SmrTradeGood::getGood($db->getInt('good_id'))->imageLink,
 			'name' => $db->getField('good_name'),
 			'amount' => $db->getInt('amount'),
 			'dump_href' => SmrSession::getNewHREF($container),
